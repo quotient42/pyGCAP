c1171dea10c4d814ea53c357df48e7fb9ef14a53
jsrim <comfortindex@naver.com>
2024-02-19 00:05:05 +0900

    [Feat] add code for making target fasta file

--- src/main/_seqlib.py
+++ src/main/_seqlib.py
@@ -2,6 +2,7 @@ import time
 import sys
 
 sys.path.append("../seqlib/")
+from make_target_fasta import make_target_fasta
 from collect_data import collect_target_data
 from blast_data import blast_data
 from search_repseq import search_repseq
@@ -10,6 +11,7 @@ from construct_lib import construct_lib
 def create_seqlib(project_info):
 	start_time = time.time()
 
+	make_target_fasta(project_info)
 	collect_target_data(project_info)
 	blast_data(project_info)
 	search_repseq(project_info)
