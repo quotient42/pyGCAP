8b12fdb3cc304cd5117886e18d9e14f5bb0b072a
jrim <comfortindex@naver.com>
2024-05-30 12:47:33 +0900

    [Update]

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='pgc-finder',
-    version='0.0.1',
-    description='python package for probe-based gene cluster finding in large microbial genome database',
+    version='0.0.2',
+    description='Python package for probe-based gene cluster finding in large microbial genome database',
     author='jsrim',
     author_email='comfortindex@naver.com',
     url='https://github.com/jrim42/gene-cluster',
-    install_requires=['pandas', 'matplotlib'],
-    packages=find_packages(exclude=[]),
+    install_requires=['pandas', 'matplotlib', 'requests'],
+    packages=find_packages(),
     keywords=[],
-    python_requires='>=3.11',
+    python_requires='>=3.6',
     package_data={},
     zip_safe=False,
     classifiers=[
@@ -18,5 +18,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
