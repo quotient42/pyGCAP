b041c470a8035c6414d82675564594b393eb01ff
jrim <comfortindex@naver.com>
2024-05-30 16:38:17 +0900

    [Rename] pgc-finder -> pyGCAP

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pgc-finder',
-    version='0.0.4',
+    name='pygcap',
+    version='0.0.1',
     description='Python package for probe-based gene cluster finding in large microbial genome database',
     author='jsrim',
     author_email='comfortindex@naver.com',
-    url='https://github.com/jrim42/pgc-finder',
+    url='https://github.com/jrim42/pyGCAP',
     install_requires=['pandas', 'matplotlib', 'requests'],
     packages=find_packages(),
     keywords=['gene', 'cluster', 'genomics', 'bioinformatics'],
     python_requires='>=3.6',
     entry_points={
         'console_scripts': [
-            'pgc-finder=pgc_finder.cli:main',
+            'pygcap=pygcap.cli:main',
         ],
     },
     zip_safe=False,
